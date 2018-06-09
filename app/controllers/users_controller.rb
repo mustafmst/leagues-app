97665b63b1f483c89cd8e11546142cda61be4903
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-06-09 12:47:39 +0200

    show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,14 @@ class UsersController < ApplicationController
   def index
   end
 
+  def login
+  end
+
   def create
+    @user = User.new(register_params)
+    @user.save
+
+    redirect_to @user
   end
 
   def new
@@ -12,6 +19,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
@@ -19,4 +27,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def register_params
+      params.require(:user).permit(:login, :password)
+    end
 end
