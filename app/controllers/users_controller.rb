3c108007f55b9873744f9de700362ba70f13843a
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-06-09 14:26:11 +0200

    basic log in functionality completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,29 +3,37 @@ class UsersController < ApplicationController
   end
 
   def login
+    @user = User.find_by login: get_login
+    if @user == nil || @user[:password] != get_password
+      flash[:error] = "Invalid login or password."
+      redirect_to login_path
+      return
+    end
+    session[:current_user_id] = @user[:id]
+    session[:current_user_name] = @user[:login]
+    redirect_to root_path
   end
 
   def get_login_form
   end
 
   def logout
-    session[:current_user] = nil
-    redirect_to '/'
+    session[:current_user_id] = nil
+    session[:current_user_name] = nil
+    @user = nil
+    redirect_to root_path
   end
 
   def create
     @user = User.new(register_params)
-
     unless @user.valid?
-      render json: {
-        status: 422,
-        message: "Validation error", 
-        errors: @user.errors    
-      }.to_json
+      flash[:error] = "Such user already exists. Try other login."
+      redirect_to new_user_path
       return
     end
-    session[:current_user] = @user.login
     @user.save
+    session[:current_user_id] = @user[:id]
+    session[:current_user_name] = @user[:login]
     redirect_to @user
   end
 
@@ -48,5 +56,13 @@ class UsersController < ApplicationController
   private
     def register_params
       params.require(:user).permit(:login, :password)
+    end
+
+    def get_login
+      params.require(:user).permit(:login)[:login]
+    end
+
+    def get_password
+      params.require(:user).permit(:password)[:password]
     end
 end
