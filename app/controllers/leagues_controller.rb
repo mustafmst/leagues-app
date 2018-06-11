ad6f1808e2156e0d55cd4babbcde2a627d1c1da0
Pawel Mstowski <pawel.mstowski@gmail.com>
2018-06-11 21:34:12 +0200

    Creating  new leagues

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,40 @@
 class LeaguesController < ApplicationController
-  def index
-    @leagues = League.all
-  end
+    def index
+        @leagues = League.all
+    end
+
+    def new
+        if session[:current_user_id] == nil
+            redirect_to root_path
+            return
+        end
+    end
+
+    def create
+        if session[:current_user_id] == nil
+            redirect_to root_path
+            return
+        end
+        if params.require(:league)[:name] == ""
+            flash[:error] = "League name is empty."
+            redirect_to new_league_path
+            return
+        end
+        @league = League.create(league_params)
+        redirect_to leagues_path
+    end
+
+    def show
+        @league = League.find(params[:id])
+    end
+
+    private
+        def league_params
+            form_params = params.require(:league)
+            return {
+                    :name => form_params[:name], 
+                    :user => User.find(session[:current_user_id]),
+                    :max_contestants => form_params[:max_contestants]
+            }
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+    # Users routes
     resources :users
     get 'login', :to => 'users#get_login_form'
     post 'login', :to => 'users#login'
     get 'logout', :to => 'users#logout'
-    get 'leagues', :to => 'leagues#index', :as => 'leagues'
     get 'users/:id/editpassword', :to => 'users#edit_password', :as => 'edit_password'
     put 'users/:id/password', :to => 'users#update_password', :as => 'update_password'
+    # Leagues routes
+    get 'leagues', :to => 'leagues#index', :as => 'leagues'
+    get 'leagues/new', :to => 'leagues#new', :as => 'new_league'
+    post 'leagues', :to => 'leagues#create', :as => 'create_league'
+    get 'leagues/:id', :to => 'leagues#show', :as => 'show_league'
+    # Other routes
     root "welcome#index"
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
